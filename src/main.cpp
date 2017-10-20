0cea14873420b1a304411d94dac84e6d5bc66a01
Fredrik Salomonsson <plattfot@gmail.com>
2017-10-20 19:27:59 +0800

    Added sources for c++ and tests

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+
+int main(int argv, char** argc)
+{
+  // Read in files from path (default to cwd)
+  // Filter out files to copy (based on extension)
+  // Get creation date on files (metadata -> filename -> current date [warn when using])
+  // Group by creation date
+  // Filter out files (already existing -> user input)
+  // Query for names (gps coord -> user input)
+  // Copy files
+  
+  return 0;
+}
---
+++ test/picmover.cpp
@@ -0,0 +1,7 @@
+#include <catch.hpp>
+#include <picmover/picmover.hpp>
+
+TEST_CASE()
+{
+  
+}
