beea8d345494c93108e2fd11a1f07a5be5edefd2
Fredrik Salomonsson <plattfot@gmail.com>
2017-10-21 15:50:47 +0800

    Fleshing out filter and grouping

--- src/picmover.hpp
+++ src/picmover.hpp
@@ -1,2 +1,72 @@
 #pragma once
 
+#include "version.hpp"
+
+#include <experimental/filesystem>
+#include <vector>
+#include <regex>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <type_traits>
+
+namespace picmover {
+inline
+namespace PICMOVER_VERSION_STR {
+  namespace fs = std::experimental::filesystem;
+  
+  using Files = std::vector<fs::path>;
+
+  struct RegexFilter{
+    RegexFilter( std::regex regex ): m_regex(regex){}
+    bool operator()( const fs::path& file ) const;
+  private:
+    std::regex m_regex;
+  };
+
+  // Read in files from path (default to cwd)
+  /// Read in all files at specifed path
+  Files read( const fs::path& path );
+
+  // Filter out based on operator
+  template< typename Filter>
+  Files filter( const Files& files, const Filter filter );
+
+  /// Group by attributes
+  template< typename Attribute>
+  auto groupBy( const Files& files, const Attribute attribute ) ->
+    std::map<std::decay_t<decltype( attribute(fs::path()) )>, Files>;
+
+  // Copy files
+  void copy( const Files& files, const fs::path& destination );
+
+
+} // namespace vX_Y
+} // namespace picmover
+
+
+namespace picmover {
+inline 
+namespace PICMOVER_VERSION_STR {
+
+  template< typename Filter>
+  Files filter( const Files& files, Filter filter )
+  {
+    Files subset;
+    std::copy_if(std::begin(files), std::end(files), std::back_inserter(subset), filter);
+    return subset;
+  }
+
+  template< typename Attribute>
+  auto groupBy( const Files& files, const Attribute attribute ) ->
+    std::map<std::decay_t<decltype( attribute(fs::path()) )>, Files>
+  {
+    std::map<std::decay_t<decltype( attribute(fs::path()) )>, Files> groups;
+
+    for( const fs::path& file : files )
+      groups[ attribute( file ) ].emplace_back( file );
+
+    return groups;
+  }
+}
+}
--- test/picmover.cpp
+++ test/picmover.cpp
@@ -1,7 +1,66 @@
 #include <catch.hpp>
 #include <picmover/picmover.hpp>
 
-TEST_CASE()
+TEST_CASE("Filter and grouping")
 {
+  picmover::Files files = {"/path/test/image.nef",
+                           "/path/test/image.raw",
+                           "/path/test/file",
+                           "/path/test/image0.jpeg",
+                           "/path/test/image1.jpeg",
+                           "/path/test/readme.txt"};
+
+  SECTION("Filter out nothing")
+  {
+    auto all_files = std::move( picmover::filter( files, []( const picmover::fs::path& )
+      {
+        return true;
+      } ));
+    
+    REQUIRE( all_files.size() == 6 );
+  }
   
+  SECTION("Filter out everything")
+  {
+    auto all_files = std::move( picmover::filter( files, []( const picmover::fs::path& )
+      {
+        return false;
+      } ));
+    
+    REQUIRE( all_files.empty() );
+  }
+
+  SECTION("Filter out nef")
+  {
+    auto nef_files =
+      std::move( picmover::filter( files, picmover::RegexFilter(std::regex("\\.nef")) ));
+    
+    REQUIRE( nef_files.size() == 1 );
+  }
+
+  SECTION("Filter out files with no extension")
+  {
+    auto ext_files =
+      std::move( picmover::filter( files, picmover::RegexFilter(std::regex("\\..*")) ));
+    
+    REQUIRE( ext_files.size() == 5 );
+  }
+
+  SECTION("Group by extensions")
+  {
+    auto exts = 
+      std::move( picmover::groupBy( files, []( const picmover::fs::path& file )
+      {
+        return file.has_extension() ? file.extension().string() : "unknown";
+      }));
+    
+    REQUIRE( exts.size() == 5 );
+
+    REQUIRE( exts["jpeg"].size() == 2 );
+    REQUIRE( exts["nef"].size() == 1 );
+    REQUIRE( exts["txt"].size() == 1 );
+    REQUIRE( exts["raw"].size() == 1 );
+    REQUIRE( exts["unknown"].size() == 1 );
+  }
 }
+
