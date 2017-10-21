bdc05c6098a722b8dcef760cc98f13832de91162
Fredrik Salomonsson <plattfot@gmail.com>
2017-10-21 15:51:21 +0800

    Added header to compute the version

---
+++ src/version.hpp
@@ -0,0 +1,14 @@
+#define PICMOVER_MAJOR 0
+#define PICMOVER_MINOR 1
+#define PICMOVER_PATCH 0
+
+// From https://github.com/dreamworksanimation/openvdb/blob/master/openvdb/version.h
+#define PICMOVER_STRINGIFY_(x) #x
+#define PICMOVER_STRINGIFY(x) PICMOVER_STRINGIFY(x)
+#define PICMOVER_CONCAT_(x,y) x ## y
+#define PICMOVER_CONCAT(x,y) PICMOVER_CONCAT_(x,y)
+
+#define PICMOVER_VERSION_STR                    \
+  PICMOVER_CONCAT(v,                            \
+  PICMOVER_CONCAT( PICMOVER_MAJOR,              \
+  PICMOVER_CONCAT( _, PICMOVER_MINOR )))
