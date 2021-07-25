3de49d362dbe03461914b23c5accffa1f23a5aff
Fredrik Salomonsson <plattfot@posteo.net>
2021-07-24 20:32:43 -0700

    Update copyright

--- picmover.py
+++ picmover.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# Copyright 2013-2014 Fredrik Salomonsson
+# Copyright 2013-2021 Fredrik Salomonsson
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
